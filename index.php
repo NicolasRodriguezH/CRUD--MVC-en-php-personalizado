69329c790ed66d3ca4db23fe273b0552fa2b317a
Nicolas <nicolashilarion420@gmail.com>
2022-02-02 11:11:03 -0500

    Inicio CRUD en MVC listo para agregar estilos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+
+    <title>NEWCRUD-MVC</title>
+</head>
+<body class="">
+
+<h1 class="bg-sky-300/50 text-2xl border text-center">Montando MVC</h1>
+<?php
+
+    include_once "controller/controller.php";
+
+?>
+    
+</body>
+</html>
